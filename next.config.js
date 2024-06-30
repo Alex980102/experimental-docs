14221cde9df018014a57eda9f57dead197e04f8f
Alex980102 <74475326+Alex980102@users.noreply.github.com>
2024-06-30 17:13:14 -0600

    Add reactStrictMode config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withNextra = require('nextra')({
   mermaid: true,
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx'
-})
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  reactStrictMode: true,
+  // Puedes agregar otras configuraciones aquí si es necesario
+});
