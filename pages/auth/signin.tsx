62461c1a8d59a485df7a0e9fe8caa102866b8b4d
Alex980102 <74475326+Alex980102@users.noreply.github.com>
2024-06-30 18:42:50 -0600

    Add responsive screen

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -9,10 +9,9 @@ const logoUrl = "https://paisuscripciones.com/website/img/pai-official-stripe-pa
 
 export default function SignIn({ csrfToken }: SignInProps) {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-50">
-      <div className="w-full max-w-sm p-6 space-y-8 bg-white rounded-xl shadow-lg">
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md p-6 space-y-8 bg-white rounded-xl shadow-lg">
         <div className="flex items-center justify-center space-x-4">
-          {/* <h1 className="text-2xl font-semibold text-gray-900">Inicia sesión</h1> */}
           <img src={logoUrl} alt="Logo" className="h-20" />
         </div>
         <form method="post" action="/api/auth/callback/credentials" className="mt-8 space-y-6">
